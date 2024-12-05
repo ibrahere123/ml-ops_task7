0fee4f7026b66ea84e5fefa8347ea0bbf45b601d
ibrahere123 <141240656+ibrahere123@users.noreply.github.com>
2024-12-06 03:15:52 +0500

    Updated test file:

--- Fullstack/Backend/tests/test_api.py
+++ Fullstack/Backend/tests/test_api.py
@@ -4,7 +4,7 @@ import os
 
 import pytest
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
-from app import app
+from Flask.app import app
 @pytest.fixture
 def client():
     with app.test_client() as client:
